bc5b0979ad1da45362757c158b38d7534c11df50
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 03:59:52 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
+   attr_reader :name, :age
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    self.class.all << self
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |m| m.customer == self }
+  end
+
+  def waiters
+    self.meals.collect { |m| m.waiter }
+  end
+
+  def self.all
+    @@all
+  end
   
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,42 @@
 class Waiter
+  attr_reader :name, :yrs_experience
+  @@all = []
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    self.class.all << self
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |m| m.waiter == self }
+  end
+
+  # def customers
+  #   self.meals.collect { |m| m.customer }
+  # end
+
+  def best_tipper
+    best_tipped_meal = self.meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+
+    best_tipped_meal.customer
+  end
+
+  def worst_tipper
+    worst_tipped_meal = self.meals.min do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+
+    worst_tipped_meal.customer
+  end
+
+  def self.all
+    @@all
+  end
 
 end
